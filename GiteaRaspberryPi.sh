3d6f976749ad5344f7320efc8df9ebc529b52980
Coşkun ERGAN <33680116+coskunergan@users.noreply.github.com>
2021-10-15 23:40:38 +0300

    Update GiteaRaspberryPi.sh
    - some fixes were made and got rid of the redundancies.

--- GiteaRaspberryPi.sh
+++ GiteaRaspberryPi.sh
@@ -1,12 +1,11 @@
 #Raspbian OS un lite versiyonu olan image dosyasını yüklüyoruz. 
 #Sadece konsol üzerinden erişeceğimiz için desktop versiyonunu tercih etmedim.
-#Aşağıdaki komutlar ile RPI cihazın güncellemelerini yükleyin.
 
+#Aşağıdaki komutlar ile RPI cihazın güncellemelerini yükleyin.
 sudo apt-get update && sudo apt-get upgrade
 
 #Aşağıdaki komut ile ayarlara ulaşıp default şifrenizi değiştirip SSH portunuzu aktif ediniz.
 #(wlan bağlantı kullananlar Wifi ayarlarını buradan yapabilirler.)
-
 sudo raspi-config 
 
 #Şimdi Docker kuruyoruz.
@@ -25,16 +24,12 @@ sudo pip3 install docker-compose
 
 #docker kurulduktan sonra docker image lerininin web üzerinden güncellenmesi ve bakımı ve performansı gözlemlemek için portainer kuruyoruz.(isteğe bağlıdır)
 sudo docker pull portainer/portainer-ce:linux-arm
-
 sudo docker run --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:linux-arm
 
-#Şimdi time zone seçelim.
+#Time zone ayarlıyoruz.
 sudo dpkg-reconfigure tzdata
 
-#NTFS dosya sistemi ile formatladığımız USB disk için ntfs-3g kuruyoruz.
-sudo apt-get install ntfs-3g
-
-#yeni bit git kullanıcı tanımlıyoruz.
+#yeni bir git kullanıcı tanımlıyoruz.
 sudo adduser --disabled-login --gecos 'Gitea' git
 
 #Gitea için oluştutulan git kullanıcısını docker a tanıtıyoruz.
@@ -47,14 +42,17 @@ sudo -u git id
 #Şimdi USB disk için bir dosya adresi oluşturup izinlerini alıyoruz.
 sudo mkdir /mnt/git
 sudo chown -R git:git /mnt/git
-sudo chmod -R 770 /mnt/git
+sudo chmod -R 775 /mnt/git
 
 #Aşağıdaki kodu çalıştırıp mount edilecek USB diskin UUID sini kopyalıyoruz.
 sudo blkid
 
 #Şimdi kopyaladığımız UUID verisi ile aşağıdaki satırı güncelliyoruz ve fstab dosyasının altına ekliyoruz.
-#UUID=00F60E97F60E8D5A     /mnt/git     ntfs-3g      uid=1001,gid=1001,umask=007 0 0
+#UUID=00F60E97F60E8D5A     /mnt/git     ntfs-3g      uid=1001,gid=1001,umask=002 0 0
 sudo nano /etc/fstab
+
+#USB cihazı mount ediyoruz.
+sudo mount -a
 
 #şimdi USB cihazın mount listesinde olup olmadığını kontrol ediyoruz.
 sudo mount -fav
@@ -65,20 +63,8 @@ sudo mount -fav
 #/                             : ignored
 #/mnt/git                      : successfully mounted <<<<< !!!!
 
-#listede göremezseniz aşağıdaki komutu çalıştırıp listeyi tekrar kontrol edebilirsiniz.
-sudo mount -a
-
-#şimdi USB diskin otomatik olarak mount edildiğini doğrulamak için restart yapıyoruz.
-sudo reboot
-
-#Aşağıdaki komutu tekrar çalıştırıp mount listesinde /mnt/git adresinin görüldüğünden emin olunuz.
-sudo mount -fav 
-
 #Gitea doker compose için açmış olduğumuz git kullanıcına atlıyoruz.
 sudo su - git 
-
-#Yeni kullanıcıya gitea için çalıştırma izni veriyoruz
-chmod +x gitea  
 
 #bir dosya oluşturuyor ve içine giriyoruz.
 mkdir gitea
@@ -86,7 +72,7 @@ cd gitea
 
 #aşağıdaki kodu çalıştırınca karşımıza nano text editör yeni bir sayfa getirecek. buraya repo içindeki "docker-compose.yml" isimli dosyanın tamamını kopyalayıp yapıştırıyoruz.
 nano docker-compose.yml
-# kaydetmek için ctrl  + X yapıyoruz daha sonra Y + Enter diyoruz.
+# kaydetmek için ctrl + O tuşluyoruz devamında enter daha sonra dosyadan çıkmak için ctrl + X tuşluyoruz.
 
 #Son olarak docker stack dosyamızı çalıştırıyoruz. 
 docker-compose up -d
